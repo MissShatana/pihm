b2005e848afc802a6bfb58f73f78c2d6f3a5eb4e
LENOVO <LENOVO@192.168.1.74>
2019-03-08 16:35:48 +0100

    LeMenu,Quitter,Menu_Items pour java doc et qqmodifs

--- src/mini_projet_ihm/Menu_Items.java
+++ src/mini_projet_ihm/Menu_Items.java
@@ -18,85 +18,70 @@ import javafx.scene.text.Font;
 import javafx.scene.text.Text;
 
 /**
- *
- * @author Chacha
+ * This class builds Items used in the menu 
+ * @author Charlotte
  */
 public class Menu_Items extends Parent{
-    public String motif;//lettre de la touche. public car lue par les autres classes
-    private int positionX,positionY;//abcsisse et ordonée de la touche  
+    public String motif;
+    private final int positionX,positionY;//abcsisse et ordonée de la touche  
     Rectangle fond_item;
     
-    
-    
+    /**
+     * This method buils a button/item used in a menu 
+     * @param l is the message displayed in the button 
+     * @param posX is the position of the button on the X axis
+     * @param posY is the position of the button on the Y axis
+     * @param url is the pathway to the icon linked to the button
+     */
     public Menu_Items(String l,int posX,int posY,String url){
         motif= new String(l);
         positionX = posX;
         positionY = posY;
         Text motif_item;
+        
+        //set an image in an icone
         Image image = new Image(Menu_Items.class.getResourceAsStream(url));
         ImageView icone=new ImageView();
         icone.setImage(image);
         icone.setFitWidth(25);
         icone.setFitHeight(25);
         
+        // add a blue background and a black strocke to put border to the button
         fond_item= new Rectangle(260,50,Color.DODGERBLUE);
         fond_item.setStroke(Color.BLACK);
         this.getChildren().add(fond_item);
         this.setTranslateX(positionX);
         this.setTranslateY(positionY);
         
+        // add a text in the button 
         motif_item = new Text(motif);
         motif_item.setFont(new Font(25));
         motif_item.setFill(Color.BLACK);
         motif_item.setX(10);
         motif_item.setY(45);
         
+        //add the icone next to the text of the button
         HBox box = new HBox();
         box.setSpacing(10);
         box.setPadding(new Insets(10, 10, 10, 10));
         box.getChildren().addAll(motif_item,icone);
         this.getChildren().add(box);
-    
+        
+        // set the position of each button 
         this.setTranslateX(positionX);
         this.setTranslateY(positionY);
+        
+        // add an event : if the mouse enter on the button it will change its color to notify this mouse position 
         this.setOnMouseEntered(new EventHandler<MouseEvent>(){
             public void handle(MouseEvent me){
                 fond_item.setFill(Color.AQUA);
             }
         });
+        // another event that show the mouse exit by taking back the first blue color
         this.setOnMouseExited(new EventHandler<MouseEvent>(){
             public void handle(MouseEvent me){
                 fond_item.setFill(Color.DODGERBLUE);
             }
-        });
-        this.setOnMousePressed(new EventHandler<MouseEvent>(){
-        public void handle(MouseEvent me){
-            appuyer();
-        }
-    });
-    
-    this.setOnMouseReleased(new EventHandler<MouseEvent>(){
-        public void handle(MouseEvent me){
-            relacher();
-        }
-    });
+        });     
     }
-    public void appuyer(){
-        fond_item.setFill(Color.AQUA);
-        this.setTranslateY(positionY+2);
-        
- 
-    }
-    
-    public void relacher(){
-        fond_item.setFill(Color.AQUA);
-        this.setTranslateY(positionY);
-        
-    }
-
-    void setOnAction(EcouteurForm e) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
-    
-    
 }
--- src/mini_projet_ihm/Quitter.java
+++ src/mini_projet_ihm/Quitter.java
@@ -5,33 +5,36 @@
  */
 package mini_projet_ihm;
 
-import javafx.event.EventHandler;
 import javafx.scene.Parent;
 import javafx.scene.control.Button;
-import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.HBox;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Rectangle;
 import javafx.scene.text.Text;
 
 /**
- *
- * @author Chacha
+ *This class create a Quit window that allows users cancel if they do not really want quit the application but quit if they really want it 
+ * @author Charlotte Fievet
  */
 public class Quitter extends Parent{
     
     Button valider,annuler;
-    
+    /**
+     * This method create the window 
+     */
     public Quitter(){
+        // add a white background with black stroke 
         Rectangle fond_fenetre = new Rectangle(330,150);
         fond_fenetre.setFill(Color.WHITE);
         fond_fenetre.setStroke(Color.BLACK);
         this.getChildren().add(fond_fenetre);
         
+        // add a question to make sure that they want to quit application 
         Text confirmation = new Text("Voulez-vous vraiment quitter l'application ?");
         confirmation.setTranslateX(20);
         confirmation.setTranslateY(50);
         
+        // add the 2 buttons Validate and cancel 
         valider = new Button("Valider");
         annuler = new Button("Annuler");
         HBox boutons = new HBox();
@@ -41,20 +44,10 @@ public class Quitter extends Parent{
         boutons.getChildren().addAll(valider,annuler);
         
         this.getChildren().addAll(confirmation,boutons);
+        
+        //set the position of the window
         this.setTranslateX(170);
         this.setTranslateY(100);
-        
-//         valider.setOnMousePressed(new EventHandler<MouseEvent>(){
-//            public void handle(MouseEvent me){
-//                    System.exit(0);
-//            }
-//         });
-//         
-//         annuler.setOnMousePressed(new EventHandler<MouseEvent>(){
-//             public void handle(MouseEvent me){
-//                trouver comment revenir en arrière 
-//             }
-//         });
     }
     
 }
