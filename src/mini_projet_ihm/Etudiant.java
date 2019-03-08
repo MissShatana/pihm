31ea1e8522f85e0f1bf6963601f58435de7ac440
Natashaa <Natashaa@172.16.235.24>
2019-03-08 16:37:21 +0100

    Merge origin/master
    Conflicts:
    	src/mini_projet_ihm/TableauEtu.java

--- src/mini_projet_ihm/Etudiant.java
+++ src/mini_projet_ihm/Etudiant.java
@@ -1,15 +1,10 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package mini_projet_ihm;
 
 import java.time.LocalDate;
-import javafx.beans.property.SimpleStringProperty;
 
 /**
- *
+ *The class builds a student
  * @author Natashaa
  */
 public class Etudiant {
@@ -17,11 +12,14 @@ public class Etudiant {
     private String prenomEtudiant;
     private String promoEtudiant;
     private LocalDate naissanceEtudiant;
-    //private SimpleStringProperty nomEtudiant;
-    //private final SimpleStringProperty lastName;
-
-      
     
+    /**
+     * This method builds a student
+     * @param nomEtu is the student's surname
+     * @param prenomEtu is the student's name
+     * @param naissanceEtu is the student's birthday
+     * @param promoEtu is the student's promo
+     */
     public Etudiant(String nomEtu, String prenomEtu, LocalDate naissanceEtu, String promoEtu){
     this.nomEtudiant = nomEtu;
     this.prenomEtudiant= prenomEtu;
@@ -32,10 +30,6 @@ public class Etudiant {
     public String getNom(){
         return nomEtudiant;
     }
-    
-//    public void setNom(String nomEtu){
-//        this.nomEtudiant= new SimpleStringProperty(nomEtu);
-//    }
     
     public String getPrenom(){
         return prenomEtudiant;
